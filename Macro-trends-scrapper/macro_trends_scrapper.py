b7affb5978fd8aa46cb27163627edbf0c5cf840e
Arad Inbar <arad.inbar@ibm.com>
2020-10-01 12:30:54 +0300

    changes

--- Macro-trends-scrapper/macro_trends_scrapper.py
+++ Macro-trends-scrapper/macro_trends_scrapper.py
@@ -12,59 +12,111 @@ import json
 from multiprocessing import Process, Value
 from concurrent.futures import ThreadPoolExecutor
 import os.path
+import requests
+import re
+
 
 global_stock_dict = {}
 
-class macrotrends_generic:
+# class yahoo_generic:
 
-    def __init__(self,stock_symbol, company_name, operation):
-        #Private methods and members
-        self.__symbol = stock_symbol
-        self.__company_name = company_name
-        self.__operation = operation
-        self.__stock_path = stock_symbol +'/' + company_name + '/' + operation
-        self.__macrotrends_list = []
+    # def __init__(self,stock_symbol, company_name, operation):
+        # #Private methods and members
+        # self.__symbol = stock_symbol
+        # self.__company_name = company_name
+        # self.__operation = operation
+        # self.__yahoo_list = []
 
 
+    # def run(self):
+        # self.__soup = self.__get_html_profile_data()
+        # self.__fill_yahoo_list(self.__soup)
+        # self.fill_global_stock_dict()
+
+    # def __get_html_profile_data(self):
+        # URL = 'https://finance.yahoo.com/quote/'+self.__symbol+'/analysis?p='+self.__symbol
+        # page = requests.get(URL)
+        # soup = BeautifulSoup(page.content, 'html.parser')
+        # return soup
+    
+            
+    # def __fill_yahoo_list(self,soup):
+        # results = soup.findAll('span')
+        # j = 0 
+        # for res in results:
+            # if (res.getText() == 'EPS Actual'):
+                # for i in range(4):
+                    # res = res.find_next('td')
+                    # self.__yahoo_list.insert(0,res.text)
+
+        
+    
+    
+    # def get_list_func(self):
+        # return self.__yahoo_list
+        
+    # def fill_global_stock_dict(self):
+        # global global_stock_dict
+
+        # if (self.__operation == 'eps'):
+            # global_stock_dict[self.__symbol][0] = self.__yahoo_list
+        # elif (self.__operation == 'net-income'):
+            # global_stock_dict[self.__symbol][1] = self.__yahoo_list
+        # else:
+            # global_stock_dict[self.__symbol][2] = self.__yahoo_list
+
+
+
+class MarketWatch_Scrapper_Financials:
+
+    def __init__(self,stock_name):
+        self.stock_name = stock_name
+    
     def run(self):
-        self.__soup = self.__get_html_profile_data(self.__stock_path)
-        self.__fill_macrotrends_list(self.__soup)
-        self.fill_global_stock_dict()
+        self.soup = self.get_html_financial_data()
+        self.fill_global_stock_dict()  
 
-    def __get_html_profile_data(self, stock_path):
-        URL = 'https://www.macrotrends.net/stocks/charts/'+stock_path
+    def get_html_financial_data(self):
+        URL = 'https://www.marketwatch.com/investing/stock/'+self.stock_name+'/financials/balance-/quarter'
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, 'html.parser')
         return soup
-    
-            
-    def __fill_macrotrends_list(self,soup):
-        results = soup.findAll("th")
-        j = 0 
-        for res in results:
-            if ( j == 1):
-                for i in range(24):
-                    res = res.find_next('td')
-                    if (( i % 2 ) == 1):
-                        self.__macrotrends_list.insert(0,res.text)
-            j = j + 1
-    
-    
-    
-    def get_list_func(self):
-        return self.__macrotrends_list
+
+
+    def generic_get_value(self,soup,str_val,str_class,num):
+        results = self.soup.findAll(str_val,{"class":str_class})[num]
+        count = 0
+        for row in results.findAll("td"):
+            if (count >=6):
+                a= str(row).split(':')[1]
+                b = a.split('}')[0]
+                if (b.find('null') != -1): 
+                    b = b.replace("null", "0")
+                res = ast.literal_eval(b) 
+            count = count + 1
+        return res 
+
+      
+
+    def get_eps_growth(self):
+        return self.generic_get_value(self.soup,"tr","childRow hidden",10)
         
+    def get_net_income_growth(self):
+        return self.generic_get_value(self.soup,"tr","childRow hidden",8)    
+    
+    def get_sales_growth(self):
+        return self.generic_get_value(self.soup,"tr","childRow hidden",0)         
+
+
     def fill_global_stock_dict(self):
         global global_stock_dict
 
-        if (self.__operation == 'eps-earnings-per-share-diluted'):
-            global_stock_dict[self.__symbol][0] = self.__macrotrends_list
-        elif (self.__operation == 'net-income'):
-            global_stock_dict[self.__symbol][1] = self.__macrotrends_list
-        else:
-            global_stock_dict[self.__symbol][2] = self.__macrotrends_list
+        global_stock_dict[self.stock_name][0] = self.get_eps_growth()
+        global_stock_dict[self.stock_name][1] = self.get_net_income_growth()
+        global_stock_dict[self.stock_name][2] = self.get_sales_growth()
 
-                
+
+              
 
 def read_stock_file(file_path):
     stock_map = {}
@@ -90,18 +142,18 @@ def write_db():
 
     for key in global_stock_dict:
 
-        eps_array = global_stock_dict[key][0]
-        net_income_array = global_stock_dict[key][1]
-        sales_array = global_stock_dict[key][2]
+        eps_growth_array = global_stock_dict[key][0]
+        net_income_growth_array = global_stock_dict[key][1]
+        sales_growth_array = global_stock_dict[key][2]
 
         # open output file for writing
         with open('stock_db.txt', 'a') as filehandle:
-            filehandle.write(key +' net_income ')
-            json.dump(net_income_array, filehandle)
-            filehandle.write(' eps ')
-            json.dump(eps_array, filehandle)
-            filehandle.write(' sales ')
-            json.dump(sales_array, filehandle)
+            filehandle.write(key +' net_income_growth ')
+            json.dump(net_income_growth_array, filehandle)
+            filehandle.write(' eps_growth ')
+            json.dump(eps_growth_array, filehandle)
+            filehandle.write(' sales_growth ')
+            json.dump(sales_growth_array, filehandle)
             filehandle.write('\n')
 
 
@@ -119,13 +171,12 @@ def iteatre_over_stock_map(stock_map):
             global_stock_dict[stock_symbol] = ['','','']
             sem = threading.Semaphore(4)
 
-            obj1 = macrotrends_generic(stock_symbol,stock_map[stock_symbol], 'net-income')
-            obj2 = macrotrends_generic(stock_symbol,stock_map[stock_symbol],'eps-earnings-per-share-diluted')
-            obj3 = macrotrends_generic(stock_symbol,stock_map[stock_symbol],'revenue')
-           
+            obj1 = MarketWatch_Scrapper_Financials(stock_symbol)
+            # obj2 = macrotrends_generic(stock_symbol,stock_map[stock_symbol],'eps-earnings-per-share-diluted')
+            # obj3 = macrotrends_generic(stock_symbol,stock_map[stock_symbol],'revenue')
             t1 =  pool.submit(obj1.run) 
-            t2 =  pool.submit(obj2.run) 
-            t3 =  pool.submit(obj3.run) 
+            # t2 =  pool.submit(obj2.run) 
+            # t3 =  pool.submit(obj3.run) 
 
             time.sleep(0.02)
             if (i % 20 == 0):
@@ -141,7 +192,9 @@ def iteatre_over_stock_map(stock_map):
     pool.shutdown(wait=True)
             
 def main():
-
+    
+    # stock1 = MarketWatch_Scrapper_Financials('prsc')
+    # stock1.run()
     stock_map = read_stock_file("all_stocks.txt")
     iteatre_over_stock_map(stock_map)
     write_db()
