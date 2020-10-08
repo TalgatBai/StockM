2a15ff2e0a592e3f09b8dd586d6487b0a7f0021d
Arad Inbar <arad.inbar@ibm.com>
2020-10-08 18:37:56 +0300

    adding multhi threading

--- Macro-trends-scrapper/V2beta/backround_breakout_thread_class.py
+++ Macro-trends-scrapper/V2beta/backround_breakout_thread_class.py
@@ -1,5 +1,5 @@
 import threading
-from selenium.webdriver.chrome.options import Options  
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,23 +13,22 @@ import datetime
 import pytz
 import concurrent.futures
 import os
- 
- 
-def send_whatsapp_message(group_name , message_to_send):
- 
+
+
+def send_whatsapp_message(group_name, message_to_send):
     options = Options()
     options.add_argument('--profile-directory=Default')
-    options.add_argument('--user-data-dir=C:/Temp/ChromeProfile') 
-    driver = webdriver.Chrome(chrome_options=options)   
+    options.add_argument('--user-data-dir=C:/Temp/ChromeProfile')
+    driver = webdriver.Chrome(chrome_options=options)
 
     driver.get("https://web.whatsapp.com/")
     wait = WebDriverWait(driver, 600)
 
     x_arg = '//span[contains(@title,' + group_name + ')]'
     group_title = wait.until(EC.presence_of_element_located((
-    By.XPATH, x_arg)))
-    print (group_title)
-    print ("Wait for few seconds")
+        By.XPATH, x_arg)))
+    print(group_title)
+    print("Wait for few seconds")
     group_title.click()
     message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
 
@@ -38,35 +37,34 @@ def send_whatsapp_message(group_name , message_to_send):
     sendbutton.click()
     driver.close()
 
-class backround_breakout_thread_class(object):
 
+class backround_breakout_thread_class(object):
 
     def __init__(self):
 
         self.stocks_set = self.__get_set_of_stocks()
         thread = threading.Thread(target=self.run)
-        thread.daemon = True  
+        thread.daemon = True
         thread.start()
         thread.join()
 
     def run(self):
-        
-        sleep_time = 600
-        while True:
-            # Do something
-            for stock_symbol in self.stocks_set:
-                yahoo_stock = YahooFinancials(stock_symbol)
-                print(stock_symbol)
-                if (self.__detect_breakout(yahoo_stock, 1.2, 0.02) == True) :
-                    msg = 'Buy alert for' + stock_symbol + ' as volume increase by :' + string(yaho.get_current_volume())
-                    send_whatsapp_message('"Stocks alerts"', msg )
-                else :
-                    send_whatsapp_message('"Stocks alerts"', 'bo breakout'+stock_symbol )    
 
+        sleep_time = 10
+        while True:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.stocks_set)) as executor:
+                executor.map(self.__run_yahoo_stock, self.stocks_set)
             time.sleep(sleep_time)
 
+    def __run_yahoo_stock(self, stock):
+
+        yahoo_stock = YahooFinancials(stock)
+        if self.__detect_breakout(yahoo_stock, 1.2, 0.001) :
+            msg = 'Buy alert for' + stock + 'at volume ' + yahoo_stock.get_current_volume()
+            send_whatsapp_message('"Stocks alerts"', msg)
+
     def __is_market_open(self, nyc_datetime):
-     
+
         if (nyc_datetime.hour < 9) or (nyc_datetime.hour > 16):
             return False
 
@@ -76,10 +74,10 @@ class backround_breakout_thread_class(object):
         if (nyc_datetime.hour == 16) and (nyc_datetime.minute > 0):
             return False
 
-        return True            
+        return True
 
     def __get_relative_time_percentage(self, nyc_datetime):
-       
+
         time_of_nasdaq_open_in_minutes = 390
         hour_difference = nyc_datetime.hour - 9
         hour_difference_in_minutes = hour_difference * 60
@@ -87,9 +85,8 @@ class backround_breakout_thread_class(object):
         sum_of_min_difference = minutes_difference + hour_difference_in_minutes
         return sum_of_min_difference / time_of_nasdaq_open_in_minutes
 
-
     def __detect_breakout(self, stock, volume_threshold, percent_threshold):
-       
+
         is_breakout = False
         nyc_datetime = datetime.datetime.now(pytz.timezone('US/Eastern'))
         market_open_flag = self.__is_market_open(nyc_datetime)
@@ -107,9 +104,9 @@ class backround_breakout_thread_class(object):
             is_breakout = True
 
         return is_breakout
-        
+
     def __get_set_of_stocks(self):
-        
+
         stocks_set = set()
         file = 'technically_valid_stocks.txt'
         try:
@@ -119,12 +116,13 @@ class backround_breakout_thread_class(object):
                     stocks_set.add(stock_symbol)
         except:
             print('Failed to read file')
-        
+
         return stocks_set
-    
+
 
 def main():
     backround_onj = backround_breakout_thread_class()
+
 
 if __name__ == "__main__":
     main()
