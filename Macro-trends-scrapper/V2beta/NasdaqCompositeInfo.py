2962f55ffab05b4eba07e4b6381e8951095e6270
Shahar <shaharman5@gmail.com>
2020-10-24 15:58:22 +0300

    rearrange functions order

--- Macro-trends-scrapper/V2beta/NasdaqCompositeInfo.py
+++ Macro-trends-scrapper/V2beta/NasdaqCompositeInfo.py
@@ -27,6 +27,24 @@ class NasdaqComposite:
         self.ma_7_change = ma_7_change
 
 
+def check_ma_3_and_ma_7_percentage_changes(ma_3_change, ma_7_change, market_notes):
+    if not ma_3_change > 0:
+        market_notes.append('MA(3) change <= 0')
+    if not ma_7_change > 0:
+        market_notes.append('MA(7) change <= 0')
+    if not ma_3_change > ma_7_change:
+        market_notes.append('MA(3) <= MA(7)')
+
+
+def check_ma_3_and_ma_7(ma_3, ma_7, market_notes):
+    if not ma_3 > ma_7:
+        market_notes.append('MA(3) <= MA(7)')
+
+
+def get_last_ma_change(first_ma, second_ma):
+    return round((second_ma - first_ma) / first_ma * 100, 2)
+
+
 def calc_ma_by_parameter(historical_prices, ma_parameter):
     sum_of_prices = 0
     for i in range(ma_parameter):
@@ -48,26 +66,6 @@ def calc_historical_prices(stock, ma_calc_end_range, trading_days_to_reach):
             return historical_prices
 
 
-def get_current_market_status():
-    if not is_trading_day(datetime.today()):
-        return None
-
-    wall_street_timezone = pytz.timezone(US_EASTERN)
-    wallstreet_local_time_now = datetime.now(wall_street_timezone).replace(tzinfo=None)
-    if wallstreet_local_time_now < MARKET_START_TIME:
-        return MarketStatus.BEFORE_MARKET_OPEN
-    elif wallstreet_local_time_now <= MARKET_END_TIME:
-        return MarketStatus.MARKET_OPEN
-    else:
-        return MarketStatus.AFTER_MARKET_CLOSED
-
-
-def is_trading_day(date_to_check):
-    is_business_day = len(bdate_range(date_to_check, date_to_check)) == 1
-    is_holiday = date_to_check in holidays.USA()
-    return is_business_day and not is_holiday
-
-
 def get_moving_avg_by_parameter_and_last_trading_day(stock, ma_parameter, day_to_calc_ma_for):
     try:
         historical_prices = calc_historical_prices(stock, day_to_calc_ma_for + timedelta(days=1), ma_parameter)
@@ -85,8 +83,24 @@ def get_7day_moving_avg(stock, day):
     return get_moving_avg_by_parameter_and_last_trading_day(stock, 7, day)
 
 
-def get_last_ma_change(first_ma, second_ma):
-    return round((second_ma - first_ma) / first_ma * 100, 2)
+def is_trading_day(date_to_check):
+    is_business_day = len(bdate_range(date_to_check, date_to_check)) == 1
+    is_holiday = date_to_check in holidays.USA()
+    return is_business_day and not is_holiday
+
+
+def get_current_market_status():
+    if not is_trading_day(datetime.today()):
+        return None
+
+    wall_street_timezone = pytz.timezone(US_EASTERN)
+    wallstreet_local_time_now = datetime.now(wall_street_timezone).replace(tzinfo=None)
+    if wallstreet_local_time_now < MARKET_START_TIME:
+        return MarketStatus.BEFORE_MARKET_OPEN
+    elif wallstreet_local_time_now <= MARKET_END_TIME:
+        return MarketStatus.MARKET_OPEN
+    else:
+        return MarketStatus.AFTER_MARKET_CLOSED
 
 
 def calc_last_trading_day(day):
@@ -97,20 +111,6 @@ def calc_last_trading_day(day):
     return day
 
 
-def check_ma_3_ma_7_changes(ma_3_change, ma_7_change, market_notes):
-    if not ma_3_change > 0:
-        market_notes.append('MA(3) change <= 0')
-    if not ma_7_change > 0:
-        market_notes.append('MA(7) change <= 0')
-    if not ma_3_change > ma_7_change:
-        market_notes.append('MA(3) <= MA(7)')
-
-
-def check_ma_3_ma_7_differences(ma_3, ma_7, market_notes):
-    if not ma_3 > ma_7:
-        market_notes.append('MA(3) <= MA(7)')
-
-
 def is_market_in_a_buy_situation(stock):
     market_notes = []
 
@@ -119,13 +119,15 @@ def is_market_in_a_buy_situation(stock):
 
     current_ma_3 = get_3day_moving_avg(stock, last_active_trading_day)
     current_ma_7 = get_7day_moving_avg(stock, last_active_trading_day)
-    check_ma_3_ma_7_differences(current_ma_3, current_ma_7, market_notes)
 
     previous_ma_3 = get_3day_moving_avg(stock, previous_active_trading_day)
     previous_ma_7 = get_7day_moving_avg(stock, previous_active_trading_day)
+
     current_ma_3_percentage_change = get_last_ma_change(previous_ma_3, current_ma_3)
     current_ma_7_percentage_change = get_last_ma_change(previous_ma_7, current_ma_7)
-    check_ma_3_ma_7_changes(current_ma_3_percentage_change, current_ma_7_percentage_change, market_notes)
+
+    check_ma_3_and_ma_7(current_ma_3, current_ma_7, market_notes)
+    check_ma_3_and_ma_7_percentage_changes(current_ma_3_percentage_change, current_ma_7_percentage_change, market_notes)
 
     print(f'Last active trading day: {last_active_trading_day.strftime("%d-%m-%Y")}')
     print(f'Previous active trading day: {previous_active_trading_day.strftime("%d-%m-%Y")}\n')
