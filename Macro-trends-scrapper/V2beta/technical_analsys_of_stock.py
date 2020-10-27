ca1f45b0b44a22b66895d3efe366bc6f0c963558
Shahar <shaharman5@gmail.com>
2020-10-27 19:28:47 +0200

    update market trading time due to winter clock

--- Macro-trends-scrapper/V2beta/technical_analsys_of_stock.py
+++ Macro-trends-scrapper/V2beta/technical_analsys_of_stock.py
@@ -36,10 +36,10 @@ breakout_stocks_file = 'breakout_stocks.txt'
 
 
 def is_market_open(nyc_datetime):
-    if (nyc_datetime.hour < 9) or (nyc_datetime.hour > 15):
+    if (nyc_datetime.hour < 8) or (nyc_datetime.hour > 15):
         return False
 
-    if (nyc_datetime.hour == 9) and (nyc_datetime.minute < 30):
+    if (nyc_datetime.hour == 8) and (nyc_datetime.minute < 30):
         return False
 
     if (nyc_datetime.hour == 15) and (nyc_datetime.minute > 0):
